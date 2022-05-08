67c65133b29be4515bf33972175d5a1f22327f40
Christina <christinaa126@gmail.com>
2022-05-07 22:28:09 -0700

    add tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -121,13 +121,13 @@ router.delete('/:id', (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: "No product found with this id!" });
+      res.status(404).json(err);
       return;
     }
 
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json({ message: "You have encountered a server error!" });
+    res.status(500).json(err);
   }
 });
 
